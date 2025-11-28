dd04baec7ca359097ae1ccf18c3834fabd471ae0
theo-vt <theo.veilleux.trinh@proton.me>
2025-11-28 18:27:48 -0500

    Core: support multiple active transactions

--- src/Mod/Sketcher/Gui/DrawSketchHandlerOffset.h
+++ src/Mod/Sketcher/Gui/DrawSketchHandlerOffset.h
@@ -417,7 +417,7 @@ private:
             return;
         }
 
-        Gui::Command::openCommand(QT_TRANSLATE_NOOP("Command", "Offset"));
+        openCommand(QT_TRANSLATE_NOOP("Command", "Offset"));
 
         // Create geos
         Obj->addGeometry(std::move(geometriesToAdd));
@@ -432,7 +432,7 @@ private:
             makeOffsetConstraint(listOfOffsetGeoIds);
         }
 
-        Gui::Command::commitCommand();
+        commitCommand();
     }
 
     void jointOffsetCurves(std::vector<int>& listOfOffsetGeoIds)
